b94e8ec6b937913f61d9d15f5045e8b2e9007572
Andres Bejarano <af.bejarano@uniandes.edu.co>
2020-02-27 13:47:06 -0500

    Fix: subida de tiempos
    Se agrega la libreria GSTU al proyecto para su conexión con el API de
    google y se genera una conexión para que los tiempos queden registrados
    en Spreadsheets.

--- Assets/Scripts/TimeRecorder.cs
+++ Assets/Scripts/TimeRecorder.cs
@@ -1,44 +1,53 @@
-﻿using System.Collections;
+﻿using GoogleSheetsToUnity;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TimeRecorder : MonoBehaviour
 {
-    System.DateTime hora = System.DateTime.Now;
+    readonly System.DateTime hora = System.DateTime.Now;
+    private readonly string sheetName = "Sheet1";
+    private readonly string sheetId = "1g5pCDZYr-p8EqvGF4jrXAPyphay-gJswoMuWivNw3kk";
     float tiempoInicial = Time.timeSinceLevelLoad;
-    float tiempoActividad1 = -1;
-    float tiempoActividad2 = -1;
-    float tiempoActividad3 = -1;
-    // 2x2 https://docs.google.com/forms/u/0/d/e/1FAIpQLSfeBI1jQTRXxycJQKozXC1FS51D_Yz1ncWILOCgvntAuk5HHw/formResponse entry.265794969 entry.339583602 entry.1770909071
-    // 6x6 https://docs.google.com/forms/u/0/d/e/1FAIpQLSfeBI1jQTRXxycJQKozXC1FS51D_Yz1ncWILOCgvntAuk5HHw/formResponse entry.1754340615 entry.1075263137 entry.17703641
-    [SerializeField] private string BASE_URL = "https://docs.google.com/forms/u/0/d/e/1FAIpQLSfeBI1jQTRXxycJQKozXC1FS51D_Yz1ncWILOCgvntAuk5HHw/formResponse";
-    IEnumerator Post (float tiempo1, float tiempo2, float tiempo3)
-    {
-        WWWForm form = new WWWForm();
-        form.AddField("entry.1754340615", "" + tiempo1);
-        form.AddField("entry.1075263137", "" + tiempo2);
-        form.AddField("entry.17703641", ""+tiempo3);
-        byte[] rawData = form.data;
-        WWW www = new WWW(BASE_URL, rawData);
-        yield return www;
-    }
-    public void send()
-    {
-        StartCoroutine(Post(tiempoActividad1, tiempoActividad2, tiempoActividad3));
-    }
+    float actividad = -1;
+    float tiempoActividad = -1;
+    List<string> data;
     public void registrarTiempoActividad1()
     {
-        tiempoActividad1 = Time.timeSinceLevelLoad - tiempoInicial;
+        if (actividad != 1)
+        {
+            actividad = 1;
+            tiempoActividad = Time.timeSinceLevelLoad - tiempoInicial;
+            Debug.Log(tiempoActividad.ToString());
+            send();
+        }
     }
     public void registrarTiempoActividad2()
     {
-        tiempoActividad2 = Time.timeSinceLevelLoad - tiempoActividad1;
+        if (actividad != 2)
+        {
+            actividad = 2;
+            tiempoActividad = Time.timeSinceLevelLoad - tiempoActividad;
+            send();
+        }
     }
     public void registrarTiempoActividad3()
     {
-        if (tiempoActividad3 == -1) {
-            tiempoActividad3 = Time.timeSinceLevelLoad - tiempoActividad2;
-            this.send();
+        if (actividad != 3)
+        {
+            actividad = 3;
+            tiempoActividad = Time.timeSinceLevelLoad - tiempoActividad;
+            send();
         }
+    }
+    private void send()
+    {
+        data = new List<string>()
+        {
+            hora.ToString(),
+            actividad.ToString(),
+            tiempoActividad.ToString().Replace(',','.')
+        };
+        GSTU_Search buscador = new GSTU_Search(sheetId, sheetName, "A2");
+        SpreadsheetManager​.Append(buscador, new ValueRange(data), null);
     }
 }
